c080d6ce7c5b7e564a79df5c74fbdeea4acb444f
SandyMrtss <bebesubi2@gmail.com>
2023-12-20 11:26:35 +0100

    Initial commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/martos/sandra/s04/t01/n01/S04T01N01MartosSandraApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.martos.sandra.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01MartosSandraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01MartosSandraApplication.class, args);
+	}
+
+}
